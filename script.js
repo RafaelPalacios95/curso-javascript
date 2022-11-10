d7c88d4f9f030de4964cd570fa418a8358a81511
RafaelPalacios95 <palacioscorona95@gmail.com>
2022-11-09 23:16:10 -0600

    hola

---
+++ script.js
@@ -0,0 +1,54 @@
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector("#calculo1");const input2 = document.querySelector("#calculo2")
+const btn= document.querySelector("#btncalcular")
+const parrafo = document.querySelector("#result")
+const form = document.getElementById("formulario")
+/*
+function btnOnClick(){
+  parrafo.innerHTML= "Resultado: " + input1.value + input2.value
+}
+
+btn.addEventListener("click", btnOnClick);
+*/
+/*
+function sumarInputValues(event){
+  console.log(event);
+  event.preventDefault();
+  parrafo.innerHTML= "Resultado: " + input1.value + input2.value
+}
+
+form.addEventListener("submit", sumarInputValues);
+*/
+
+//Para que una funcion se ejecute en un evento de click y si esta en un formulario, debe de ser de tipo button
+
+/*const p = document.querySelector('p');
+const parrafito = document.querySelector('.parrafito');
+const pid = document.getElementById('pid');
+const input = document.querySelector('input');*/
+/*
+console.log(input.value);
+
+console.log({h1, p, parrafito, pid, input})
+
+h1.innerText="Patito <br> Feo";
+
+console.log(h1.getAttribute("class"))
+
+//h1.setAttribute("class", "rojo")
+
+h1.classList.add("rojo")
+h1.classList.remove("verde")
+//h1.classList.toggle("verde") //quita o pone la clases
+//h1.classList.contains("verde") true o false si existe el elemento
+
+
+input.value = "453"
+
+const img = document.createElement("img");
+img.setAttribute("src", "ff33.png")
+console.log(img)
+
+pid.innerHTML = "";
+pid.appendChild(img);
+*/
